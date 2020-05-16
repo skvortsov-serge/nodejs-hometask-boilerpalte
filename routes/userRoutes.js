580ecd31e49621322238bd045c6f62ebe74cd53a
Sergey Skvortsov <skvortsov.serge@gmail.com>
2020-05-16 18:29:06 +0300

    add REST for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,6 +20,67 @@ router.post('/', createUserValid, (req, res, next) => {
   }
 }, responseMiddleware);
 
-// TODO: Implement route controllers for user
+router.get('/', (req, res, next) => {
+  try {
+    const data = UserService.getAllUsers();
+
+    if (!data)
+      throw Error('No users found');
+
+    res.data = data;
+  } catch(err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.get('/:id', (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = UserService.getUser(id);
+
+    if (!user)
+      throw Error('No user found');
+
+    res.data = user;
+  } catch(err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.put('/:id', (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = UserService.update(id, req.body);
+    
+    if (!data)
+      throw Error('Something went wrong while updating user');
+
+    res.data = data;
+  } catch(err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.delete('/:id', (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = UserService.delete(id);
+
+    if (!data)
+      throw Error('Something went wrong while deleting user');
+
+    res.data = data;    
+  } catch(err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
 
 module.exports = router;
